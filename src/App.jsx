dc425db378d10ed0bc678494cdedcfe9baf4cf91
Alan Supla-ag Oliveros <71147809+alanoliveros@users.noreply.github.com>
2024-12-04 15:51:15 +0800

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.scss'
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
-import CustomScrollbar from "./components/CustomScrollbar.jsx";
 
 function App() {
 
     return (
         <div className="app">
-            <CustomScrollbar/>
             <Header/>
             <main className="main-content">
             </main>
