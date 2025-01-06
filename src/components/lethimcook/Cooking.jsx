22160484cb8801c37b8f394593f4bd64548a2c00
Alan Supla-ag Oliveros <71147809+alanoliveros@users.noreply.github.com>
2025-01-06 13:25:31 +0800

    feat(lethimcook): add comment and remove `Try.js`
    - Added a comment to `Cooking.jsx`
    - Removed the file `Try.js`

--- src/components/lethimcook/Cooking.jsx
+++ src/components/lethimcook/Cooking.jsx
@@ -4,6 +4,7 @@ function Cooking() {
       <h1>Cooking</h1>
       <p>Let's cook!</p>
        {/* // git commit --date='2024-12-27' -m 'Pag sure uy!' */}
+       {/* // git commit --date='2025-01-01' -m 'Again' */}
     </div>
   );
 }
