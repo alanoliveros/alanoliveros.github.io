2c325f7134480e28f762d8a2107d63f243778c2e
Alan Supla-ag Oliveros <71147809+alanoliveros@users.noreply.github.com>
2025-01-06 18:50:05 +0800

    Ehh, Sheeesh!

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,14 @@ function Header() {
     return (
         <header>
             <div className="logo">Alan Gwapo</div>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Project</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
         </header>
     )
 }
