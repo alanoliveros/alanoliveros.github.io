89acf69dcbcbaca6cc55c86042b4299e009162ca
Alan Supla-ag Oliveros <71147809+alanoliveros@users.noreply.github.com>
2025-02-05 22:01:14 +0800

    refactor(Cooking.jsx): Remove unnecessary paragraph and update commit message placeholders

--- src/components/lethimcook/Cooking.jsx
+++ src/components/lethimcook/Cooking.jsx
@@ -3,9 +3,8 @@ function Cooking() {
     <div>
       <h1>Cooking</h1>
       <p>Let&#39;s cook!</p>
-      <p>Oks</p>
        {/* // git commit --date='2024-12-27' -m 'Pag sure uy!' */}
-       {/* // git commit --date='2025-01-01' -m 'Again' */}
+       {/* // git commit --date='2025-02-04' -m 'Again' */}
     </div>
   );
 }
