d4a0ce71fbe8d8217ffe6586b5b5b88765a338d8
Alan Supla-ag Oliveros <71147809+alanoliveros@users.noreply.github.com>
2025-02-05 21:57:41 +0800

    fix(Cooking.jsx): Escape single quote in paragraph text
    - Replaced a single quote in the paragraph text with its HTML entity (`&#39;`) to prevent rendering issues.
    - Added a new paragraph with the text "Oks".

--- src/components/lethimcook/Cooking.jsx
+++ src/components/lethimcook/Cooking.jsx
@@ -2,7 +2,8 @@ function Cooking() {
   return (
     <div>
       <h1>Cooking</h1>
-      <p>Let's cook!</p>
+      <p>Let&#39;s cook!</p>
+      <p>Oks</p>
        {/* // git commit --date='2024-12-27' -m 'Pag sure uy!' */}
        {/* // git commit --date='2025-01-01' -m 'Again' */}
     </div>
